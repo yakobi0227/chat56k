154ef99720496bfa8d73eab5f5e38cdf312be560
Jakey <jgquaife@gmail.com>
2026-08-31 19:58:24 -0700

    Add tagline: anybody could be anybody.

--- client/src/tags.ts
+++ client/src/tags.ts
@@ -4,4 +4,5 @@ export const TAGS = [
   "Now you can use us while your mom waits for that important call.",
   "Cable internet? Check out Mr./Mrs. Rockefeller over here.",
   "Back to an era when things didn't suck.",
+  "Where anybody could be anybody and everyone just went with it.",
 ];
